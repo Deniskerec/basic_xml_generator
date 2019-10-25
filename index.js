88339fec19853fb4709061849cf995d54adfc83a
Denis Kerec <ddeniskerec@gmail.com>
2019-10-25 10:59:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 var builder = require('xmlbuilder');
  
-var xml = builder.create('root', {encoding: 'UTF-8'})
-  .ele('scmartCardTransacttionLog')
-    .ele('DeviceId', '1232431')
-    .ele('DeviceId', '1232431')
+var xml = builder.create('smartCardTransactionLog', {encoding: 'UTF-8'})
+  .ele('deviceInformation')
+    .ele('deviceType', 'officePos').up()
+    .ele('deviceVersion', '19.09.17').up()
+    .ele('deviceID', '002406F05264').up()
+    .ele('deviceTransactionCounter', '1').up()
+    .ele('faretableTimestamp', '2019-10-09T06:24:00.963Z').up()
+    .ele('revenueMode', 'inService').up()
+    .ele('operationMode', 'issuing').up()
+    .ele('operatorTypeExternalID', '1').up()
+    .ele('operatorTypeName', 'a').up()
+    .ele('publicTransportOrganizationRef', 'Hubli').up()
   .end({ pretty: true});
  
 console.log(xml);
+
+ /*.ele('deviceInformation', '').up()
+.ele('deviceType', 'officePos').up()
+.ele('locationInformation', '').up()
+.ele('financialTransaction', '').up()
+.ele('validationInformation', '').up()
+.ele('cardReference', '').up()
+.ele('cardSnapshot', '').up()
+.ele('cardOperation', '').up()
+.ele('cardHolderInfoBasic', '').up()
+.ele('cardHolderInfoExtended', '').up()*/
